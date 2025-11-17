07f97efb594443f7614a860c71f286fc79a46f43
Emmanuel Addo Odame <eaodame@hubtel.com>
2025-11-17 05:16:56 +0000

    fix jwt-cpp dependency issue

--- main.cc
+++ main.cc
@@ -1,8 +1,8 @@
 #include <drogon/drogon.h>
 int main() {
     //Set HTTP listener address and port
-    //drogon::app().loadConfigFile("../config.json");
-    drogon::app().loadConfigFile("config.json");
+    drogon::app().loadConfigFile("../config.json");
+    //drogon::app().loadConfigFile("config.json");
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
