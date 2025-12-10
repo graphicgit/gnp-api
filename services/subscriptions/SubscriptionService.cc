50ab6c98839113a23b9f9973a6d398b0ef989e76
Emmanuel Addo Odame <eaodame@hubtel.com>
2025-12-10 15:32:15 +0000

    fix chrono days

--- services/subscriptions/SubscriptionService.cc
+++ services/subscriptions/SubscriptionService.cc
@@ -498,7 +498,7 @@ void SubscriptionService::completeGuestOneTimeBuy(
                           .set_type("JWT")
                           .set_issued_at(std::chrono::system_clock::now())
                           .set_expires_at(std::chrono::system_clock::now() +
-                                          std::chrono::days(30))
+                                          std::chrono::hours(24*30))
                           .set_payload_claim("userId",
                                              jwt::claim(user.getValueOfId()))
                           .set_payload_claim(
