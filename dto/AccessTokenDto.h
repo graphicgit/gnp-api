9f71f5a4682df82c41d15cbff5da86501d21df5f
Emmanuel Addo Odame <eaodame@hubtel.com>
2026-03-08 11:09:13 +0000

    Add Bcrypt and jwt-cpp as submodules

---
+++ dto/AccessTokenDto.h
@@ -0,0 +1,8 @@
+//
+// Created by Emmanuel Addo-Odame on 05/03/2026.
+//
+
+#ifndef GNPAPI_ACCESSTOKENDTO_H
+#define GNPAPI_ACCESSTOKENDTO_H
+
+#endif //GNPAPI_ACCESSTOKENDTO_H
---
+++ services/mtn_api/MtnMomoApi.h
@@ -0,0 +1,8 @@
+//
+// Created by Emmanuel Addo-Odame on 05/03/2026.
+//
+
+#ifndef GNPAPI_MTNMOMOAPI_H
+#define GNPAPI_MTNMOMOAPI_H
+
+#endif //GNPAPI_MTNMOMOAPI_H
